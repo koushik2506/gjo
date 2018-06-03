0db05b481721cbf89c24291239aee6444bb5d934
Koushik <koushik.narayanan@gmail.com>
2018-06-03 22:07:29 +0530

    First commit, basic key value parsing

---
+++ j.go
@@ -0,0 +1,151 @@
+//
+// gjo - An attempt of a go-version of jo
+// author - koushik.narayanan@gmail.com
+//
+
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Version struct {
+	Program string `json:"program"`
+	Author  string `json:"author"`
+	Repo    string `json:"repo"`
+	Version string `json:"version"`
+}
+
+var version Version = Version{Program: "gjo", Author: "nkoushik", Repo: "https://github.com/koushik2506/gjo", Version: "0.1"}
+
+var out io.Writer = os.Stdout
+var err io.Writer = os.Stderr
+var in io.Reader = os.Stdin
+
+var arrayflag bool
+var boolflag bool
+var verflag bool
+var jsonverflag bool
+var prettyflag bool
+
+func init() {
+	flag.BoolVar(&arrayflag, "a", false, "creates an array of words")
+	flag.BoolVar(&boolflag, "B", false, "disables boolean true/false")
+	flag.BoolVar(&verflag, "v", false, "Show version")
+	flag.BoolVar(&jsonverflag, "V", false, "Show version in JSON")
+	flag.BoolVar(&prettyflag, "p", false, "pretty-prints JSON on output")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), `Usage: jo [-a] [-B] [-p] [-v] [-V] [word...]
+word is a key=value or key@value`)
+		fmt.Fprintf(flag.CommandLine.Output(), "\n")
+		flag.PrintDefaults()
+	}
+
+}
+
+func GuessValue(input string) interface{} {
+	// try bool first if -B is not set
+	if !boolflag {
+		b, err := strconv.ParseBool(input)
+		if err == nil {
+			return b
+		}
+	}
+
+	// try int
+	i, err := strconv.ParseInt(input, 0, 64)
+	if err == nil {
+		return i
+	}
+
+	//try float
+	f, err := strconv.ParseFloat(input, 64)
+	if err == nil {
+		return f
+	}
+
+	//give up
+	return input
+}
+
+func JsonKeyValEncode(input []string) ([]byte, error) {
+	inputmap := make(map[string]interface{})
+	for _, args := range input {
+		kvArray := strings.Split(args, "=")
+		if len(kvArray) != 2 {
+			return nil, fmt.Errorf("Invalid key/value combination")
+		}
+		inputmap[kvArray[0]] = GuessValue(kvArray[1])
+	}
+
+	return JsonEncode(inputmap)
+}
+
+func JsonEncode(input interface{}) ([]byte, error) {
+	if prettyflag {
+		data, err := json.MarshalIndent(input, "", "   ")
+		return data, err
+	} else {
+		data, err := json.Marshal(input)
+		return data, err
+	}
+}
+
+func ReadInput() []string {
+	scanner := bufio.NewScanner(in)
+
+	var inputstr []string
+
+	for scanner.Scan() {
+		inputstr = append(inputstr, scanner.Text())
+	}
+
+	return inputstr
+}
+
+func main() {
+	flag.Parse()
+
+	if verflag {
+		fmt.Fprintf(out, "%s %s\n", version.Program, version.Version)
+		return
+	}
+
+	if jsonverflag {
+		data, err := JsonEncode(version)
+		if err != nil {
+			log.Fatalf("JSON Marshalling failed: %s", err)
+			return
+		}
+		fmt.Fprintf(out, "%s\n", data)
+		return
+	}
+
+	var input []string
+
+	if flag.NArg() > 0 {
+		input = flag.Args()
+	} else {
+		input = ReadInput()
+	}
+
+	data, err := JsonKeyValEncode(input)
+	if err != nil {
+		log.Fatalf("JSON Marshalling failed: %s", err)
+		return
+	}
+	fmt.Fprintf(out, "%s\n", data)
+	return
+
+	flag.Usage()
+
+}
